de52a8d05e4acc533fda65d631d2540dd9b94604
Rafael Longhi <rafaelhlonghi@gmail.com>
2015-11-28 15:26:35 -0200

    Adicionado atributos data, total, usuario e itens na classe Venda.

--- src/main/java/br/univel/model/Venda.java
+++ src/main/java/br/univel/model/Venda.java
@@ -9,6 +9,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Column;
 import javax.persistence.Version;
 import java.lang.Override;
+import java.sql.Date;
+import br.univel.model.Usuario;
+import javax.persistence.ManyToOne;
+import javax.persistence.FetchType;
+import br.univel.model.Item;
+import java.util.Set;
+import java.util.HashSet;
+import javax.persistence.ManyToMany;
 
 @Entity
 @Table(name = "venda")
@@ -22,6 +30,18 @@ public class Venda implements Serializable
    @Version
    @Column(name = "version")
    private int version;
+
+   @Column(name = "data")
+   private Date data;
+
+   @Column(name = "total")
+   private Double total;
+
+   @ManyToOne(fetch = FetchType.LAZY)
+   private Usuario usuario;
+
+   @ManyToMany
+   private Set<Item> itens = new HashSet<Item>();
 
    public Long getId()
    {
@@ -41,15 +61,6 @@ public class Venda implements Serializable
    public void setVersion(final int version)
    {
       this.version = version;
-   }
-
-   @Override
-   public String toString()
-   {
-      String result = getClass().getSimpleName() + " ";
-      if (id != null)
-         result += "id: " + id;
-      return result;
    }
 
    @Override
@@ -81,5 +92,54 @@ public class Venda implements Serializable
       int result = 1;
       result = prime * result + ((id == null) ? 0 : id.hashCode());
       return result;
+   }
+
+   public Date getData()
+   {
+      return data;
+   }
+
+   public void setData(Date data)
+   {
+      this.data = data;
+   }
+
+   public Double getTotal()
+   {
+      return total;
+   }
+
+   public void setTotal(Double total)
+   {
+      this.total = total;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (total != null)
+         result += "total: " + total;
+      return result;
+   }
+
+   public Usuario getUsuario()
+   {
+      return this.usuario;
+   }
+
+   public void setUsuario(final Usuario usuario)
+   {
+      this.usuario = usuario;
+   }
+
+   public Set<Item> getItens()
+   {
+      return this.itens;
+   }
+
+   public void setItens(final Set<Item> itens)
+   {
+      this.itens = itens;
    }
 }
