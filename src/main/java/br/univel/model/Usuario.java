f93346817ef6fac18f2302f370600f5ae91aa8cc
Rafael Longhi <rafaelhlonghi@gmail.com>
2015-11-28 14:55:48 -0200

    Adicionando atributos nome, login, senha e email na classe Usuário.

--- src/main/java/br/univel/model/Usuario.java
+++ src/main/java/br/univel/model/Usuario.java
@@ -23,6 +23,18 @@ public class Usuario implements Serializable
    @Column(name = "version")
    private int version;
 
+   @Column(length = 100, name = "nome", nullable = false)
+   private String nome;
+
+   @Column(length = 20, name = "login", nullable = false)
+   private String login;
+
+   @Column(length = 20, name = "senha", nullable = false)
+   private String senha;
+
+   @Column(length = 100, name = "email", nullable = false)
+   private String email;
+
    public Long getId()
    {
       return this.id;
@@ -41,15 +53,6 @@ public class Usuario implements Serializable
    public void setVersion(final int version)
    {
       this.version = version;
-   }
-
-   @Override
-   public String toString()
-   {
-      String result = getClass().getSimpleName() + " ";
-      if (id != null)
-         result += "id: " + id;
-      return result;
    }
 
    @Override
@@ -80,6 +83,61 @@ public class Usuario implements Serializable
       final int prime = 31;
       int result = 1;
       result = prime * result + ((id == null) ? 0 : id.hashCode());
+      return result;
+   }
+
+   public String getNome()
+   {
+      return nome;
+   }
+
+   public void setNome(String nome)
+   {
+      this.nome = nome;
+   }
+
+   public String getLogin()
+   {
+      return login;
+   }
+
+   public void setLogin(String login)
+   {
+      this.login = login;
+   }
+
+   public String getSenha()
+   {
+      return senha;
+   }
+
+   public void setSenha(String senha)
+   {
+      this.senha = senha;
+   }
+
+   public String getEmail()
+   {
+      return email;
+   }
+
+   public void setEmail(String email)
+   {
+      this.email = email;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (nome != null && !nome.trim().isEmpty())
+         result += "nome: " + nome;
+      if (login != null && !login.trim().isEmpty())
+         result += ", login: " + login;
+      if (senha != null && !senha.trim().isEmpty())
+         result += ", senha: " + senha;
+      if (email != null && !email.trim().isEmpty())
+         result += ", email: " + email;
       return result;
    }
 }
