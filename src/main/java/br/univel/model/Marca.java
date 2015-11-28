b6debee3499c57665f42e8ae5fe753148ad54e7c
Rafael Longhi <rafaelhlonghi@gmail.com>
2015-11-28 14:05:19 -0200

    Adicionado atributo nome na classe Marca.

--- src/main/java/br/univel/model/Marca.java
+++ src/main/java/br/univel/model/Marca.java
@@ -23,6 +23,9 @@ public class Marca implements Serializable
    @Column(name = "version")
    private int version;
 
+   @Column(length = 50, name = "nome", nullable = false)
+   private String nome;
+
    public Long getId()
    {
       return this.id;
@@ -41,15 +44,6 @@ public class Marca implements Serializable
    public void setVersion(final int version)
    {
       this.version = version;
-   }
-
-   @Override
-   public String toString()
-   {
-      String result = getClass().getSimpleName() + " ";
-      if (id != null)
-         result += "id: " + id;
-      return result;
    }
 
    @Override
@@ -80,6 +74,25 @@ public class Marca implements Serializable
       final int prime = 31;
       int result = 1;
       result = prime * result + ((id == null) ? 0 : id.hashCode());
+      return result;
+   }
+
+   public String getNome()
+   {
+      return nome;
+   }
+
+   public void setNome(String nome)
+   {
+      this.nome = nome;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (nome != null && !nome.trim().isEmpty())
+         result += "nome: " + nome;
       return result;
    }
 }
